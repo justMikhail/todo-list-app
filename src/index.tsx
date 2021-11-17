6af0f372c0e4c3db37c8eb125ff57ff981a81480
Mikhail Sitnikov <mikhail.walle@gmail.com>
2021-11-17 18:34:49 +0300

    Update basic structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+//import {configureStore} from '@reduxjs/toolkit';
+
+//import {createAPI} from './services/api';
+
 import App from './App';
+
 import './styles/styles.scss';
+
+// const api = createAPI();
+//
+// const store = configureStore({
+//   reducer: rootReducer,
+//   middleware: (getDefaultMiddleware) =>
+//     getDefaultMiddleware({
+//       thunk: {
+//         extraArgument: api,
+//       },
+//     }),
+// });
 
 ReactDOM.render(
   <React.StrictMode>
